466043ea79a73628cbb79db9a69132ff60c5a42a
michaeljuren <michael16.uren@gmail.com>
2026-01-30 09:10:48 +0200

    initial commit

---
+++ src/main/java/com/michaelj/crem/job_mon_backend/JobMonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.michaelj.crem.job_mon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaelj/crem/job_mon_backend/JobMonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelj.crem.job_mon_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
